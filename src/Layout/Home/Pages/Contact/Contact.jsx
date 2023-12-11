4d2ff4df10722bdc3ef68b7030de8f6b90f4bb61
Tanbir Haque <tanbirhaque53@gmail.com>
2023-12-11 22:59:08 +0600

    contact page completed

--- src/Layout/Home/Pages/Contact/Contact.jsx
+++ src/Layout/Home/Pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-
+import { MdArrowRightAlt } from "react-icons/md";
 
 const Contact = () => {
     return (
@@ -22,15 +22,49 @@ const Contact = () => {
                         </div>
                         <div className="border-[#3d3d47] border-b-[1px] pb-2 flex justify-between mt-[8px]">
                             <span className="bg-[#61ce70] text-gray-900 text-[14px] px-[10px] max-h-[22px]">Email:</span>
-                            <span>tanbirhaque53@gmail.com</span>
+                            <span className="text-[14px]">tanbirhaque53@gmail.com</span>
                         </div>
                         <div className="border-[#3d3d47] border-b-[1px] pb-2 flex justify-between mt-[8px]">
                             <span className="bg-[#61ce70] text-gray-900 text-[14px] px-[10px] max-h-[22px]">Phone:</span>
                             <span>+8001646740414</span>
                         </div>
                         <div className="border-[#3d3d47] border-b-[1px] pb-2 flex justify-between mt-[8px]">
-                            <span className="bg-[#61ce70] text-gray-900 text-[14px] px-[10px] max-h-[22px]">Email:</span>
-                            <span>tanbirhaque53@gmail.com</span>
+                            <span className="bg-[#61ce70] text-gray-900 text-[14px] px-[10px] max-h-[22px]">Freelance:</span>
+                            <span>Available</span>
+                        </div>
+                    </div>
+
+                    <div className="contact_section">
+                        <div className="mx-[30px] mb-5">
+                            <h3 className="font-bold text-[20px] text-white popFont"><span className="text-[#61ce70]">C</span>ontact Form</h3>
+                        </div>
+                        <div className="divider m-0 h-0"></div>
+                        <div className="px-[30px] pt-[30px]">
+                            <form>
+                                <div className="grid grid-cols-2 gap-7">
+                                    <input
+                                        type="text"
+                                        placeholder="Full Name"
+                                        className="py-[10px] pl-[15px] bg-transparent border-[2px] border-[#666] text-[#61ce70] focus:outline-none focus:ring-2 focus:ring-[#61ce70] focus:border-none"
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="Email Address"
+                                        className="py-[10px] pl-[15px] bg-transparent border-[2px] border-[#666] text-[#61ce70] focus:outline-none focus:ring-2 focus:ring-[#61ce70] focus:border-none"
+                                    />
+                                </div>
+                                <div>
+                                    <textarea
+                                        type="text"
+                                        placeholder="Your Message"
+                                        className="w-full h-[100px] p-[15px] bg-transparent border-[2px] border-[#666] text-[#61ce70] focus:outline-none focus:ring-2 focus:ring-[#61ce70] focus:border-none"
+                                    />
+                                </div>
+                                <div className="flex items-center mt-6">
+                                    <p className="text-white uppercase font-semibold text-[14px]">Send Message</p>
+                                    <MdArrowRightAlt className="text-[26px] text-white"/>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
